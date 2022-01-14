74c40ad7082cc93b88370959c317913f584cb78b
Zach Krall <2532937+zachkrall@users.noreply.github.com>
2022-01-13 20:02:55 -0500

    wip

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # msdv-dvia-fall-2021
+
+
+## requirements 
+
+* Node.js 16.11.1
+* Yarn 
--- cms/utils/files.ts
+++ cms/utils/files.ts
@@ -26,34 +26,3 @@ export const formatFileName = (str) => {
     .replace(/^.*\/secure.notion-static.com\//g, '')
     .replace(/[^A-Za-z0-9-_.]/g, '-')
 }
-
-export const setupImageFolder = async (folderPath) => {
-  let exists = await folderExists(folderPath)
-
-  if (!exists) {
-    await fs.mkdir(folderPath)
-  }
-
-  console.log(`[✓] Set up image directory: ${chalk.gray(folderPath)}`)
-
-  let files = await fs.readdir(folderPath)
-
-  files.forEach(async (filePath) => {
-    let fullPath = path.join(folderPath, filePath)
-    try {
-      await fs.unlink(fullPath)
-      console.log(`${chalk.bgGray.white(' DELETE ')} ${chalk.gray(filePath)}`)
-    } catch (e) {
-      console.log(`${chalk.bgRed.black(' ERROR ')} ${filePath}`)
-    }
-  })
-}
-
-export const setupApiJSON = async (filePath) => {
-  const data = JSON.stringify({
-    projects: [],
-  })
-  await fs.writeFile(filePath, data, { encoding: 'utf-8' })
-
-  console.log(`[✓] Set up api file: ${chalk.gray(filePath)}`)
-}
